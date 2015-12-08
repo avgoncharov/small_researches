c88eb2035b306884763da7f84109f92c4f1833f0
avgoncharov <goncharov.andrey.v@gmail.com>
2015-12-08 17:07:35 +0300

    Добавил проект с примером как хостить WCF службу в консоли.

---
+++ researches/WcfInConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace WcfInConsole
+{
+	class Program
+	{
+		static void Main()
+		{
+			using (var host = new ServiceHost(typeof(WCF.TestService)))
+			{
+				host.Open();
+			
+				Console.WriteLine("Press <Enter> to stop the service.");
+				Console.ReadLine();
+
+				host.Close();
+			}
+		}
+	}
+}
