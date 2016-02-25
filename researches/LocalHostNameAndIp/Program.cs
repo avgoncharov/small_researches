adc645a3282da0a37ec02f668e6e60b252b7353c
avgoncharov <goncharov.andrey.v@gmail.com>
2016-02-25 10:49:13 +0300

    Мелкие ислодования по получению Ip и HostName.

---
+++ researches/LocalHostNameAndIp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace LocalHostNameAndIp
+{
+	public class Program
+	{
+		public  static void Main()
+		{
+			var hostName = Dns.GetHostName();
+			Console.WriteLine(hostName);
+
+			IPHostEntry entry = Dns.GetHostEntry(hostName);
+			Console.WriteLine(entry.HostName);
+
+			foreach (var ip in entry.AddressList.Where(x=>x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork))
+			{
+				Console.WriteLine(ip);
+			}
+			Console.ReadLine();
+		}
+	}
+}
